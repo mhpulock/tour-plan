d1e3ab979fea3427f0c13f07b3d3a918984af2a9
mhpulock <ippolok24@gmail.com>
2021-10-31 23:28:27 +0600

    first commit

---
+++ src/hooks/useService.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react"
+
+const useService = () => {
+
+    const [service, setService] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
+    return [service, setService];
+}
+export default useService;
